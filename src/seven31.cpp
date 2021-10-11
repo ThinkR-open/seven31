5b6c64bae848791c19380edee6d2a50ce11cc040
Romain Francois <romain@rstudio.com>
2021-10-11 14:30:07 +0200

    remove Rcpp dependency

--- src/seven31.cpp
+++
@@ -1,8 +0,0 @@
-#include <Rcpp.h>
-using namespace Rcpp;
-
-// [[Rcpp::export]]
-IntegerVector parts(double x) {
-  int* p = reinterpret_cast<int*>(&x) ;
-  return IntegerVector::create( p[0], p[1] ) ;
-}
